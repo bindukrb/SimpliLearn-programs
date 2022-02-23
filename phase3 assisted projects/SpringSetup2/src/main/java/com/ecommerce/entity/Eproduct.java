c483cd44c6f58dcf0e34fb21b25ccefd1ac38451
bindukrb <97723947+bindukrb@users.noreply.github.com>
2022-02-23 16:23:27 +0530

    phase 3 assisted programs

---
+++ phase3 assisted projects/SpringSetup2/src/main/java/com/ecommerce/entity/Eproduct.java
@@ -0,0 +1,32 @@
+package com.ecommerce.entity;
+
+import java.math.BigDecimal;
+
+public class Eproduct {
+	
+    private long ID;
+    private String name;
+    private BigDecimal price;
+    
+	public long getID() {
+		return ID;
+	}
+	public void setID(long iD) {
+		ID = iD;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public BigDecimal getPrice() {
+		return price;
+	}
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+    
+    
+
+}
---
+++ phase3 assisted projects/SpringSetup2/src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,10 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<body>
+<h2>Spring Application</h2>
+
+<a href="listProducts">List Products</a>
+</body>
+</html>
